205080a5a67b92031bec5134410b74d29afd1fdc
Kendry Grullon <kengrullon@gmail.com>
2024-08-21 15:15:02 -0400

    Initial config

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.New("index.html").ParseFiles("templates/layout.html", "templates/index.html")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		err = t.ExecuteTemplate(w, "layout", "")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+
+	mux.HandleFunc("POST /generate", func(w http.ResponseWriter, r *http.Request) {
+		text := r.FormValue("string")
+		log.Println(text)
+		http.Redirect(w, r, "/", http.StatusFound)
+	})
+
+	http.ListenAndServe(":9876", mux)
+}
