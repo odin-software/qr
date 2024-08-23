c37f569de317222db2bc99e1db00a18b90e88124
Kendry Grullon <kengrullon@gmail.com>
2024-08-23 15:04:23 -0400

    Getting encoding mode.

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ func main() {
 	mux.HandleFunc("POST /generate", func(w http.ResponseWriter, r *http.Request) {
 		text := r.FormValue("string")
 		log.Println(text)
+		log.Println(GetEncodingMode(text))
 		http.Redirect(w, r, "/", http.StatusFound)
 	})
 
--- qr.go
+++ qr.go
@@ -1,1 +1,25 @@
 package main
+
+type EncodingMode uint8
+
+const (
+	NUMERIC_MODE EncodingMode = 1 << iota
+	ALPHA_MODE
+	BYTE_MODE
+)
+
+func GetEncodingMode(text string) EncodingMode {
+	var r = 0b111
+	for _, t := range text {
+		if t > 57 || t < 48 {
+			r = r &^ int(NUMERIC_MODE)
+		}
+		if t >= 'a' && t <= 'z' {
+			r = r &^ int(ALPHA_MODE)
+		}
+	}
+	return EncodingMode(r)
+}
+
+func CreateDataSegment(text string) {
+}
