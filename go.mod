1630112314bdf978742c78a426ceea434b1412ac
Kendry Grullon <kengrullon@gmail.com>
2024-08-26 20:25:45 -0400

    Adding libraries

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module github.com/odin-software/qr
 
 go 1.22.0
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/yeqown/go-qrcode/v2 v2.2.4
+	github.com/yeqown/go-qrcode/writer/standard v1.2.4
+)
+
+require (
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/yeqown/reedsolomon v1.0.0 // indirect
+	golang.org/x/image v0.19.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/yeqown/go-qrcode/v2"
+	"github.com/yeqown/go-qrcode/writer/standard"
 )
+
+const IMAGE_NAME = "imgName"
+const STATIC_DIR = "static"
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServerDir := fmt.Sprintf("./%s", STATIC_DIR)
+	fileServerPattern := fmt.Sprintf("GET /%s/", STATIC_DIR)
+	fileServerStrip := fmt.Sprintf("/%s/", STATIC_DIR)
+
+	fs := http.FileServer(http.Dir(fileServerDir))
+	mux.Handle(fileServerPattern, http.StripPrefix(fileServerStrip, fs))
 
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		t, err := template.New("index.html").ParseFiles("templates/layout.html", "templates/index.html")
@@ -21,10 +36,40 @@ func main() {
 		}
 	})
 
+	mux.HandleFunc("GET /{imgName}", func(w http.ResponseWriter, r *http.Request) {
+		q := r.PathValue(IMAGE_NAME)
+		url := fmt.Sprintf("%s/%s.png", STATIC_DIR, q)
+		t, err := template.New("index.html").ParseFiles("templates/layout.html", "templates/index.html")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		err = t.ExecuteTemplate(w, "layout", url)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+
 	mux.HandleFunc("POST /generate", func(w http.ResponseWriter, r *http.Request) {
 		text := r.FormValue("string")
-		CreateDataSegment(text)
-		http.Redirect(w, r, "/", http.StatusFound)
+		qrc, err := qrcode.New(text)
+		if err != nil {
+			fmt.Printf("could not generate QRCode: %v", err)
+			return
+		}
+		id := uuid.New()
+		filePath := fmt.Sprintf("%s/%s.png", STATIC_DIR, id.String())
+		wr, err := standard.New(filePath)
+		if err != nil {
+			fmt.Printf("standard.New failed: %v", err)
+			return
+		}
+		if err = qrc.Save(wr); err != nil {
+			fmt.Printf("could not save image: %v", err)
+		}
+		redirectUrl := fmt.Sprintf("/%s", id.String())
+		http.Redirect(w, r, redirectUrl, http.StatusFound)
 	})
 
 	http.ListenAndServe(":9876", mux)
